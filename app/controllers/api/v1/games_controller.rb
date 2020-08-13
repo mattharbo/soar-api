9446d126471dab3f2761275ffc01f4c4db93ca66
Matthieu Harbonnier <matthieu.harbonnier@gmail.com>
2020-08-13 16:26:36 +0200

    Game/:id route response first draft

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -4,7 +4,25 @@ class Api::V1::GamesController < SecuredController
   before_action :find_game, only: [:show]
 
   def show
-		render json: @game
+
+    render :json => {
+      # "Competition":
+      "Game details": {
+        stage: @game.round,
+        date: @game.date,
+        # time: @game.time, #==> Not ready yet! find a way to submit a time
+        status: @game.gstate.description
+      },
+      "Participants": [
+          hometeam: @game.hometeam.club.name,
+          awayteam: @game.awayteam.club.name,
+          hashtag: @game.hometeam.club.acronym+@game.awayteam.club.acronym
+      ]
+      # "Result": {
+      #   home: @game.,
+      # }
+    }
+
 	end
 
   private
